ddf0fb452add1ed612bd3416a08be90d225ff2d0
David <yemmy072@gmail.com>
2022-08-24 11:19:52 +0100

    change

---
+++ grades.js
@@ -0,0 +1,42 @@
+const studentsResults = [{
+        {
+            id: 'Tobi',
+            Mathematics: 45,
+            English: 54,
+            Chemistry: 46,
+            ComputerScience: 78,
+        },
+        {
+            id: "Solutions",
+            Mathematics: 68,
+            English: 75,
+            Chemistry: 43,
+            ComputerScience: 90
+        }, 
+        {
+            id:'Dada',
+            Mathematics: 67,
+            English: 23,
+            Chemistry: 85,
+            ComputerScience: 21
+        },
+      {
+            id: 'Jide',
+            Mathematics: 89,
+            English: 12,
+            Chemistry: 33,
+            ComputerScience: 43
+        },
+       
+      {
+            id:"Ralph",
+            Mathematics: 32,
+            English: 14,
+            Chemistry: 23,
+            ComputerScience: 43
+        },
+]
+
+studentsResults.map((element, ) => {
+    console.log(element, element[i])
+})
